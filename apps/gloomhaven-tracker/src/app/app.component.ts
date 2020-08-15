170b940b6129f394c30d03440fc1e597bf5aeb3c
Peter Sokolík <pesoklp13@gmail.com>
2020-08-15 19:12:36 +0200

    Initial project setup
    nx angular application and changed favicon.ico for GH

---
+++ apps/gloomhaven-tracker/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gloomhaven-tracker-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gloomhaven-tracker';
+}
